d8a7dea8654dc8fa35ef053852997b3e86ea1f99
Zasenko <dmitry.zasenko@gmail.com>
2024-12-30 08:57:12 +0100

    tests

--- tests.c
+++ tests.c
@@ -2,11 +2,28 @@
 
 void *print_message(void *arg);
 void *get_time(void *arg);
+void *increment(void *arg);
+void *increment2(void *arg);
+void mutex_test(void);
+
+void test_kill(void);
+void test_pthread_create(void);
+void test_pthread_detach(void);
+
+pthread_mutex_t lock;
+int counter = 0;
 
 void tests(void)
 {
-    write(1, "Hello, world!\n", 14);
+    //test_kill();
+    //test_pthread_create();
+    //test_pthread_detach();
+    mutex_test();
+    printf("END\n");
+}
 
+void test_kill(void)
+{
     //----------------------------------------------
     printf("-- kill --\n");
     //----------------------------------------------
@@ -42,8 +59,11 @@ void tests(void)
         printf("Отправляем сигнал SIGKILL дочернему процессу\n");
         kill(pid, SIGKILL); // Убиваем дочерний процесс
     }
-   
-   //----------------------------------------------
+}
+
+void test_pthread_create(void)
+{
+    //----------------------------------------------
     printf("-- pthread_create --\n");
     //----------------------------------------------
     // #include <pthread.h>
@@ -60,7 +80,7 @@ void tests(void)
     if (pthread_create(&thread, NULL, print_message, (void *)message) != 0)
     {
         perror("pthread_create");
-        return ;
+        return;
     }
     // Ожидаем завершения потока
     printf("1\n");
@@ -71,28 +91,92 @@ void tests(void)
     //	• retval — указатель на значение, возвращённое функцией потока.
 
     void *result;
-    pthread_join(thread, &result);
+    pthread_join(thread, &result); // Ожидает завершения указанного потока
     printf("Результат потока: %s\n", (char *)result);
+}
 
+void test_pthread_detach(void)
+{
     //----------------------------------------------
     printf("-- pthread_detach --\n");
     //----------------------------------------------
-    pthread_t thread2;
+    // Переводит поток в отделённое состояние.
+    // Это освобождает его ресурсы автоматически после завершения.
+    // 1. Поток создаётся и немедленно отделяется.
+    // 2. Основной поток завершает работу, не ожидая завершения отделённого потока.
+    pthread_t thread;
     const char *message2 = "Hello from detached thread!";
-    if (pthread_create(&thread2, NULL, get_time, (void *)message2) != 0)
+    if (pthread_create(&thread, NULL, get_time, (void *)message2) != 0)
     {
         perror("Ошибка при создании потока");
         return;
     }
-    printf("Основной поток 2.\n");
+    printf("Основной поток - начало\n");
     pthread_detach(thread);
 
-    printf("Основной поток 2 Начало паузы\n");
-    sleep(2); // Пауза на 500000 микросекунд (0.5 секунды)
-    printf("Основной поток 2 Конец паузы\n");
-    printf("Основной поток 2 завершён.\n");
+    printf("Основной поток  Начало паузы\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("основной %d\n", i);
+        sleep(1); // Пауза на 500000 микросекунд (0.5 секунды)
+    }
+        
+    printf("Основной поток - Конец паузы\n");
+    printf("Основной поток - завершён.\n");
+}
 
-    printf("END\n");
+void mutex_test(void)
+{
+    //----------------------------------------------
+    printf("-- mutex --\n");
+    //----------------------------------------------
+    // int pthread_mutex_init(pthread_mutex_t * mutex, const pthread_mutexattr_t *attr);
+    // • mutex — указатель на мьютекс.
+    // • attr — атрибуты мьютекса(передать NULL для стандартных атрибутов).
+
+    // int pthread_mutex_init(pthread_mutex_t * mutex, const pthread_mutexattr_t *attr);
+    // int pthread_mutex_destroy(pthread_mutex_t * mutex);
+    // int pthread_mutex_lock(pthread_mutex_t *mutex);
+    // int pthread_mutex_unlock(pthread_mutex_t * mutex);;
+    pthread_t thread1, thread2;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&thread1, NULL, increment, "1");
+    pthread_create(&thread2, NULL, increment2, "2");
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Итоговое значение счётчика: %d\n", counter);
+
+    pthread_mutex_destroy(&lock);
+}
+
+void *increment(void *arg)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        pthread_mutex_lock(&lock);
+        counter++;
+        printf("Поток %s (%p) увеличил счётчик до %d\n", (char *)arg, (void *) pthread_self(), counter);
+        pthread_mutex_unlock(&lock);
+        sleep(1);
+    }
+    return NULL;
+}
+
+void *increment2(void *arg)
+{
+    for (int i = 0; i < 20; i++)
+    {
+        pthread_mutex_lock(&lock);
+        counter++;
+        printf("Поток %s (%p) увеличил счётчик до %d\n", (char *)arg, (void *)pthread_self(), counter);
+        pthread_mutex_unlock(&lock);
+        usleep(100000);
+    }
+    return NULL;
 }
 
 void *print_message(void *arg)
@@ -117,10 +201,16 @@ void *get_time(void *arg)
 {
     printf("-- get_time --\n");
     printf("get_time принято: %s\n", (char *)arg);
-    printf("get_time Начало паузы\n");
-    for (int i = 0; i < 100000000; i++)
-        printf("%d get_time work\n", i);
-    printf("get_time Конец паузы\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("не основной%d\n", i);
+        sleep(2); // Пауза на 500000 микросекунд (0.5 секунды)
+    }
+    // printf("get_time Начало паузы\n");
+    // for (int i = 0; i < 100000000; i++)
+    //     printf("%d get_time work\n", i);
+    // printf("get_time Конец паузы\n");
     struct timeval tv;
     gettimeofday(&tv, NULL);
     printf("get_time Секунды: %ld, Микросекунды: %d\n", tv.tv_sec, tv.tv_usec);
