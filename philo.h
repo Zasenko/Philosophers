29ffbc10ea2eedfd56e862a3ec66d1276bfce36d
Dmitry Zasenko <dzasenko@c2r3p9.42vienna.com>
2024-12-30 11:09:54 +0100

    add headers to main.c + philo.h

--- philo.h
+++ philo.h
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzasenko <dzasenko@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 11:06:24 by dzasenko          #+#    #+#             */
+/*   Updated: 2024/12/30 11:08:41 by dzasenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <pthread.h>
-#include <sys/time.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <signal.h>
+# include <pthread.h>
+# include <sys/time.h>
 
-void tests(void);
+void	tests(void);
 
 #endif
